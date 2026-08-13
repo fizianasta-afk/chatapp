e14a476362b475856ba6e1c7edc02f6616f521cd
fizianasta-afk <fizianasta@gmail.com>
2026-08-13 10:06:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,32 +2,45 @@ const input = document.querySelector(".message-box input");
 
 const messages = document.querySelector(".messages");
 
+const sendButton = document.querySelector("#sendButton");
+
+
+function sendMessage() {
+
+    const text = input.value.trim();
+
+    if (text === "") {
+        return;
+    }
+
+    const message = document.createElement("div");
+
+    message.className = "message sent";
+
+    message.innerHTML = `
+        ${text}
+        <small>
+            Baru saja ✓
+        </small>
+    `;
+
+    messages.appendChild(message);
+
+    input.value = "";
+
+    messages.scrollTop = messages.scrollHeight;
+}
+
+
+sendButton.addEventListener("click", sendMessage);
+
+
 input.addEventListener("keydown", function(event) {
 
     if (event.key === "Enter") {
 
-        const text = input.value.trim();
+        sendMessage();
 
-        if (text === "") {
-            return;
-        }
-
-        const message = document.createElement("div");
-
-        message.className = "message sent";
-
-        message.innerHTML = `
-            ${text}
-            <small>
-                Baru saja ✓
-            </small>
-        `;
-
-        messages.appendChild(message);
-
-        input.value = "";
-
-        messages.scrollTop = messages.scrollHeight;
     }
 
 });
